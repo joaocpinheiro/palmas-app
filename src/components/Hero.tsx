251c6b7b78bfe1766b1ca823d87ac61122803283
Joao Carlos <joaocpinheiro03@gmail.com>
2024-01-11 23:07:10 -0300

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section className="pt-24 pb-12 xl:py-0 h-[85vh] xl:h-[1087px] bg-hero2 xl:bg-hero bg-cover bg-center bg-no-repeat flex justify-center items-center">
+      Hero
+    </section>
+  )
+}
+
+export default Hero
